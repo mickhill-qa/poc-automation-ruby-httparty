cd7d516071f61de52f19fd3137d4efa6425f097c
Mick Hill - MacBook Pro M1 <mickhill@hotmail.com.br>
2022-02-21 12:56:50 -0300

    Atualizando gerador de relatórios

--- features/support/report_builder.rb
+++ features/support/report_builder.rb
@@ -1,13 +1,13 @@
 ## Gerar relatorio ao final da execucao
 at_exit do
   ReportBuilder.configure do |config|
-    config.json_path        = "reports/report.json"                                                                                   # pasta onde salva o json
-    config.report_path      = REPORT_PATH + Time.now.strftime('%Y-%m-%d_-_%H-%M-%S_-_').to_s + 'api-test' + '_' + SELECTED_ENV                           # pasta onde salva o html
-    config.report_types     = [:html]                                                                                                 # tipo de report a exportar
-    config.report_title     = "poc-automation-ruby-httparty"                                                                                       # nome do report - <img src='#' />
-    config.color            = "blue"                                                                                                  # cor do report
+    config.json_path        = "reports/report.json"                                                                              # pasta onde salva o json
+    config.report_path      = REPORT_PATH + Time.now.strftime('%Y-%m-%d_-_%H-%M-%S_-_').to_s + SELECTED_ENV + "_" + 'api'        # pasta onde salva o html
+    config.report_types     = [:html]                                                                                            # tipo de report a exportar
+    config.report_title     = "poc-automation-ruby-httparty"                                                                     # nome do report - <img src='#' />
+    config.color            = "orange"                                                                                             # cor do report
     config.compress_images  = false
-    config.include_images   = true                                                                                                    # coloca imagens ou não
+    config.include_images   = true                                                                                               # coloca imagens ou não
     config.additional_info  = {
       'Date': Time.now,
       'Environment': SELECTED_ENV,
